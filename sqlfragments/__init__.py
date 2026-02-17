72e951f713d4d86e7692f770e8dc765e8625e128
Jennifer Taylor <dragonminded@dragonminded.com>
2026-02-17 03:32:48 +0000

    Export public interface.

---
+++ sqlfragments/__init__.py
@@ -0,0 +1,20 @@
+from ._fragments import (
+    FragmentException,
+    InvalidFragment,
+    InvalidSpecifier,
+    InvalidName,
+    MissingArgument,
+    Fragment,
+    fragment,
+)
+
+
+__all__ = [
+    "FragmentException",
+    "InvalidFragment",
+    "InvalidSpecifier",
+    "InvalidName",
+    "MissingArgument",
+    "Fragment",
+    "fragment",
+]
